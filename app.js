172f58f65055442cde544551a2e0025923cd9793
ginnygu <ginnygu@gmail.com>
2023-05-17 11:07:13 -0400

    added movie.js file and changed response of index route to json movie

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const movies = require("./movies");
+
 const app = express();
 const PORT = 3000;
+
+app.get("/", (req, res) => {
+	res.status(200).json({ data: movies });
+});
+app.get("/about", (req, res) => {
+	res.status(200).send("from about page");
+});
 
 app.listen(PORT, () => {
 	console.log(`listening to port ${PORT}`);
