e47b240bdb36c476ab5f2fd06b08e33e3edeb872
ginnygu <ginnygu@gmail.com>
2023-05-17 15:55:44 -0400

    added genre route, delete route and started req.query /actors route

--- app.js
+++ app.js
@@ -19,13 +19,19 @@ app.get("/action", (req, res) => {
 	res.status(200).json({ data: actionFilms });
 });
 
-//dynamic, :<req.params:name>
+//dynamic genre route, :<req.params:name>
 app.get("/genre/:genreName", (req, res) => {
 	//http://localhost:3000/genre/action
 	// console.log(req.params); //{ genreName: 'action' }
 	// console.log(req.params.genreName); // action
+
+	//http://localhost:3000/genre/drama
+	// console.log(req.params); //{ genreName: 'drama' }
+	// console.log(req.params.genreName); // drama
+
 	const genre = req.params.genreName.toLowerCase();
 
+	//explicit returns uses the word return
 	const filteredByGenre = movies.filter((film) => {
 		return film.Genre.toLowerCase().includes(genre);
 	});
@@ -34,13 +40,34 @@ app.get("/genre/:genreName", (req, res) => {
 });
 // update the app.get genre to filter movies based on the genreName we pass in and respond back to the browser with the list of the filtered films.
 
+//make a GET route that finds all films after the year 2010
+//make a GET route that finds one film based on Title
+//make a GET route that finds films based on the imdbRating using two numbers -Hint: Chain params
+
+// Delete = DELETE request
+app.delete("/delete-movie/:imdbID", (req, res) => {
+	//.indexOf
+	const imdbID = req.params.imdbID;
+	//implicit returns
+	const findIndexOfMovie = movies.findIndex((film) => film.imdbID === imdbID);
+	//.splice(index, how many)
+	movies.splice(findIndexOfMovie, 1);
+	// console.log(movies);
+	res.status(200).json({ data: "movie deleted" });
+});
+
+//req.query
+app.get("/actors", (req, res) => {
+	//http://localhost:3000/actors?n=lena
+	console.log(req.query); //{ n: 'lena' }
+});
+
 app.get("/about", (req, res) => {
 	res.status(200).send("from about page");
 });
 
 // Create = POST Request
-// Update = PUT Request/ Patch
-// Delete = DELETE request
+// Update = PUT Request/ Patch Request
 
 app.listen(PORT, () => {
 	console.log(`listening to port ${PORT}`);
